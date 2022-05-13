9649606cced54e987a5e821bbcaa26980c4d5a76
Monica Craveiro <craveiromonica@gmail.com>
2022-05-13 12:13:07 -0300

    Create schemas from mongoose.

--- Routes/users.js
+++ Routes/users.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Users = require('../model/user');
 
 router.get('/', (req, res) => {
-    return res.send({
-        message: 'Tudo ok com o méodo GET da rota de usuários.'
-    })
-});
-
-router.post('/', (req, res) => {
-    return res.send({
-        message: 'Tudo ok com o méodo POST da rota de usuários.'
-    })
+    Users.find({}, (err, data) => {
+        if (err) return res.send({ error: 'Erro na consulta de usuários.' });
+        
+        return res.send(data);
+    });
 });
 
 router.post('/create', (req, res) => {
-    return res.send({
-        message: 'Seu usuário foi criado.'
-    })
+    // const obj = req.body;
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.send({ error: 'Dados Insuficientes.' });
+    
+    Users.findOne({ email }, (err, data) => {
+        if (err) return res.send({ error: 'Erro ao buscar usuário.' });
+        if (data) return res.send({ error: 'Usuário já cadastrado' });
+        
+        // Users.create({email: email, password: password}, (err, data) => {});
+        Users.create(req.body, (err, data) => {
+            if (err) return res.send({ error: 'Erro ao criar usuário.' });
+
+            data.password = undefined;
+            return res.send(data);
+        });
+    });
 });
 
 module.exports = router;
