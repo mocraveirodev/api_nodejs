0f106d2d7a0c784be0cc659162da426c65cef2e4
Monica Craveiro <craveiromonica@gmail.com>
2022-05-13 00:50:38 -0300

    Aula 1

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    let obj = req.query;
+
+    return res.send({
+        message: `Tudo OK com o método GET. Você enviou o nome ${obj.nome} com idade de ${obj.idade} anos.`
+    });
+});
+
+app.post('/', (req, res) => {
+    return res.send({
+        message: "Tudo OK com o método POST."
+    });
+});
+
+app.listen('3000');
+
+module.exports = app;
