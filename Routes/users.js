f4c89302663e2b150ffaeaba877ae0a5817dc153
Monica Craveiro <craveiromonica@gmail.com>
2022-05-13 14:06:24 -0300

    Use bcrypt to encrypt password.

--- Routes/users.js
+++ Routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('../model/user');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     Users.find({}, (err, data) => {
@@ -23,11 +24,29 @@ router.post('/create', (req, res) => {
         // Users.create({email: email, password: password}, (err, data) => {});
         Users.create(req.body, (err, data) => {
             if (err) return res.send({ error: 'Erro ao criar usuário.' });
-
+            
             data.password = undefined;
             return res.send(data);
         });
     });
 });
+
+router.post('/auth', (req, res) => {
+    const { email, password } = req.body;
+    
+    if (!email || !password) return res.send({ error: 'Dados Insuficientes.' });
+
+    Users.findOne({ email }, (err, data) => {
+        if (err) return res.send({ error: 'Erro ao buscar usuário.' });
+        if (!data) return res.send({ error: 'Usuário não registrado.' });
+
+        bcrypt.compare(password, data.password, (err, same) => {
+            if (!same) return res.send({ error: 'Erro ao autenticar usuário.' });
+
+            data.password = undefined;
+            return res.send(data);
+        });
+    }).select('+password');
+})
 
 module.exports = router;
--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     email: {
@@ -17,6 +18,18 @@ const UserSchema = new Schema({
         type: Date,
         default: Date.now
     }
+});
+
+//Não é possivel usar arrow function pois há problemas de escopo com o this.
+UserSchema.pre('save', function (next) {
+    let user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, 10, (err, encrypted) => {
+        user.password = encrypted;
+        return next();
+    })
 });
 
 module.exports = mongoose.model('User', UserSchema);
